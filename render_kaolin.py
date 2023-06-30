f93530eddfa4082c22bd2f493c32889382b56452
Tomas Jakab <tomas.jakab.64@gmail.com>
2023-06-30 21:09:13 +0100

    update render kaolin code

--- render_kaolin.py
+++ render_kaolin.py
@@ -10,6 +10,8 @@ import numpy as np
 import kaolin as kal
 import math
 
+# use makeit3d conda env
+
 
 def render(obj_path, camera_matrix_path, out_img_path):
     mesh = kal.io.obj.import_mesh(obj_path, with_materials=True)
@@ -43,35 +45,40 @@ def render(obj_path, camera_matrix_path, out_img_path):
     camera_up_direction = torch.tensor([[0, 1, 0]], dtype=torch.float32, device='cuda')
     camera_transform = kal.render.camera.generate_transformation_matrix(camera_position, look_at, camera_up_direction)
 
-    # face_vertices_camera, face_vertices_image, face_normals = \
-    #     kal.render.mesh.prepare_vertices(
-    #         vertices_batch.repeat(batch_size, 1, 1),
-    #         faces, cam_proj, camera_transform=cam_transform
-    #     )
-
-    camera_transform
-    padded_vertices = torch.nn.functional.pad(
-        vertices, (0, 1), mode='constant', value=1.
-    )
-    # Project the vertices on the camera image plan
-    # vertices_camera = (padded_vertices @ camera_transform)
-
+    # Convert the camera matrix from Blender to OpenGL coordinate system
     conversion_mat = torch.Tensor([[1, 0, 0, 0],
                                 [0, 0, 1, 0],
                                 [0, -1, 0, 0],
                                 [0, 0, 0, 1]]).float().to('cuda')
     camera_matrix = conversion_mat @ camera_matrix
 
+    # Convert the camera matrix to view matrix
     view_matrix = camera_matrix.inverse()[:3]
-    padded_vertices_T = padded_vertices.permute(0, 2, 1)
-    vertices_camera = view_matrix @ padded_vertices_T
-    vertices_camera = vertices_camera.permute(0, 2, 1)
 
+    # # Column major
+    # padded_vertices = torch.nn.functional.pad(
+    #     vertices, (0, 1), mode='constant', value=1.
+    # )
     # Project the vertices on the camera image plan
-    vertices_image = kal.render.camera.perspective_camera(vertices_camera, cam_proj)
-    face_vertices_camera = kal.ops.mesh.index_vertices_by_faces(vertices_camera, faces)
-    face_vertices_image = kal.ops.mesh.index_vertices_by_faces(vertices_image, faces)
-    face_normals = kal.ops.mesh.face_normals(face_vertices_camera, unit=True)
+    # padded_vertices_T = padded_vertices.permute(0, 2, 1)
+    # vertices_camera = view_matrix @ padded_vertices_T
+    # vertices_camera = vertices_camera.permute(0, 2, 1)
+
+    # # Project the vertices on the camera image plan
+    # vertices_image = kal.render.camera.perspective_camera(vertices_camera, cam_proj)
+    # face_vertices_camera = kal.ops.mesh.index_vertices_by_faces(vertices_camera, faces)
+    # face_vertices_image = kal.ops.mesh.index_vertices_by_faces(vertices_image, faces)
+    # face_normals = kal.ops.mesh.face_normals(face_vertices_camera, unit=True)
+
+    # Row major
+    # Blender is column major, but kaolin is row major
+    view_matrix = view_matrix.T
+
+    face_vertices_camera, face_vertices_image, face_normals = \
+    kal.render.mesh.prepare_vertices(
+        vertices_batch.repeat(batch_size, 1, 1),
+        faces, cam_proj, camera_transform=view_matrix
+    )
 
     ### Perform Rasterization ###
     # Construct attributes that DIB-R rasterizer will interpolate.
