4494a592b3c57b98c35085364938adfdcba5564c
Tomas Jakab <tomas.jakab.64@gmail.com>
2023-07-10 16:19:06 +0100

    accept any number of configs

--- main.py
+++ main.py
@@ -7,21 +7,15 @@ if __name__ == "__main__":
     # get config path from command line
     parser = argparse.ArgumentParser(description="Train the model")
     # Add arguments to the parser
-    parser.add_argument("config", type=str, help="Path to the config file")
-    parser.add_argument(
-        "config_secondary",
-        type=str,
-        help="Path to the secondary config file such as a test config (will be merged with the main config and override the main config)))",
-    )
+    parser.add_argument("configs", type=str, nargs="+", help="Path to the config file")
     # Parse the arguments
     args = parser.parse_args()
 
     # load config with omegaconf and instantiate trainer with hydra
-    # TODO: use for loop to load multiple configs (any number of configs)
-    config_path = args.config
-    config = OmegaConf.load(config_path)
-    if args.config_secondary is not None:
-        config_secondary = OmegaConf.load(args.config_secondary)
+    config_paths = args.configs
+    config = OmegaConf.load(config_paths[0])
+    for config_path in config_paths[1:]:
+        config_secondary = OmegaConf.load(config_path)
         config = OmegaConf.merge(config, config_secondary)
 
     trainer = instantiate(config)
