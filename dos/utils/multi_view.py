9581d71c4dee7c8abcf987d327049d01636cf007
Oishi Deb <oishideb@login1.triton.cluster>
2024-02-27 21:25:10 +0000

    updated for debugging purpose

--- dos/utils/multi_view.py
+++ dos/utils/multi_view.py
@@ -145,3 +145,80 @@ def rand_poses(size, device, theta_range=[0, 120], phi_range=[0, 360], uniform_s
     poses, dirs = poses_helper_func(size, device, phis, thetas, radius_range=radius_range)
     
     return poses, dirs
+
+
+
+
+# Added for debugging purpose
+def poses_helper_func_single_view(size, device, phis, thetas, radius_range=[1, 1], angle_overhead=30, angle_front=60, phi_offset=0, jitter=False, cam_z_offset=12, return_dirs=True):
+
+    angle_overhead = np.deg2rad(angle_overhead)
+
+    angle_front = np.deg2rad(angle_front)
+    
+    radius = torch.rand(size, device=device) * (radius_range[1] - radius_range[0]) + radius_range[0]
+
+    targets = torch.zeros(size, 3, device=device)
+
+    centers = -torch.stack([
+        radius * torch.sin(thetas) * torch.sin(phis),
+        radius * torch.cos(thetas),
+        radius * torch.sin(thetas) * torch.cos(phis),
+    ], dim=-1) # [B, 3]
+    
+    # jitters
+    if jitter:
+        centers = centers + (torch.rand_like(centers) * 0.2 - 0.1)
+        targets = targets + torch.randn_like(centers) * 0.2
+    
+    # lookat
+    forward_vector = safe_normalize(targets - centers)
+    up_vector = torch.FloatTensor([0, 1, 0]).to(device).unsqueeze(0).repeat(size, 1)
+    right_vector = safe_normalize(torch.cross(up_vector, forward_vector, dim=-1))
+    
+    if jitter:
+        up_noise = torch.randn_like(up_vector) * 0.02
+    else:
+        up_noise = 0
+    
+    up_vector = safe_normalize(torch.cross(forward_vector, right_vector, dim=-1) + up_noise)
+
+    poses = torch.stack([right_vector, up_vector, forward_vector], dim=-1)
+    radius = radius[..., None] - cam_z_offset
+
+    translations = torch.cat([torch.zeros_like(radius), torch.zeros_like(radius), radius], dim=-1) # Original
+
+    poses = torch.cat([poses.view(-1, 9), translations], dim=-1)
+
+    if return_dirs:
+        dirs = get_view_direction(thetas, phis, angle_overhead, angle_front, phi_offset=phi_offset)
+        dirs = view_direction_id_to_text(dirs)
+    else:
+        dirs = None
+
+    return poses, dirs
+
+def poses_along_azimuth_single_view(size, device, theta=90, phi_range=[0, 360]):
+    ''' generate random poses from an orbit camera along uniformly distributed azimuth and fixed elevation
+    Args:
+        size: batch size of generated poses.
+        device: where to allocate the output.
+        theta: is a constant                          
+        phi_range: [min, max], should be in [0, 2 * pi]
+    Return:
+        poses: [size, 4, 4]
+    '''
+    theta = np.deg2rad(theta)                   
+    phi_range = np.deg2rad(phi_range)
+    
+    # For azimuth rotation (phi), we will create a sequence of values within the specified range
+    phis = torch.linspace(phi_range[0], phi_range[1], steps=size, device=device)
+    
+    # Keeping theta (elevation angle) constant
+    thetas = torch.full((size,), theta, device=device)
+
+    # targets = torch.zeros(size, 3, device=device)
+
+    poses, dirs = poses_helper_func_single_view(size, device, phis, thetas)
+
+    return poses, dirs
